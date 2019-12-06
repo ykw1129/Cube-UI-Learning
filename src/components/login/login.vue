539eb65a6d76fc6e30ec57e51d01e390e2bca7ca
ykw1129 <1794093413@qq.com>
2019-12-06 09:19:19 +0800

    卸载掉cube-ui

--- src/components/login/login.vue
+++ src/components/login/login.vue
@@ -1,15 +1,18 @@
 <template>
     <div id="login">
-
+        <cube-button :light="true">Light Button</cube-button>
+        <cube-button :inline="true">Inline Button</cube-button>
+        <cube-button :outline="true">Outline Button</cube-button>
+        <cube-button :primary="true">Primary Button</cube-button>
     </div>
 </template>
 
 <script>
-export default {
+    export default {
 
-}
+    }
 </script>
 
-<style>
-    
+<style lang="stylus" scoped>
+
 </style>
