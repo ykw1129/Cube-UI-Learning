6e1b89ac95446829e69ed33de78bb5ca0b3e7e82
ykw <1794093413@qq.com>
2019-12-12 16:05:07 +0800

    add router

--- src/main.js
+++ src/main.js
@@ -3,11 +3,13 @@ import './cube-ui'
 import App from './App.vue'
 import Cube from 'cube-ui'
 import 'amfe-flexible'
+import router from './router'
 
 Vue.use(Cube)
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
